6b9e9a1c2d9552ba9d085d4e087834de5a516f46
patricia <patricia@10.0.29.245>
2017-03-07 16:19:57 -0300

    Primeiro Commit

---
+++ ProjetoNike/src/br/inatel/ec206/model/Vestuario.java
@@ -0,0 +1,12 @@
+package br.inatel.ec206.model;
+
+public class Vestuario {
+	
+	private String camiseta;
+	private String camisaDeTime;
+	private String polo;
+	private String shorts;
+	private String calcaELegging;
+	
+
+}
